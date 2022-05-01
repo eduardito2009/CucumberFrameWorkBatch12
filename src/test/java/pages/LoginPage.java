27d2e7ff39b45ea7dc5b642b3043363755c19d95
eduardito2009 <cm655176@gmail.com>
2022-05-01 15:02:07 -0400

    CumberFrameWork

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.CommonMethods;
+
+public class LoginPage extends CommonMethods {
+    //This is object repository
+    @FindBy(id="txtUsername")
+    public WebElement usernameBox;
+
+    @FindBy(id="txtPassword")
+    public WebElement passwordBox;
+
+    @FindBy(id="btnLogin")
+    public  WebElement loginBtn;
+
+    public LoginPage(){
+
+        PageFactory.initElements(driver,this);//initialize the elements
+    }
+
+}
